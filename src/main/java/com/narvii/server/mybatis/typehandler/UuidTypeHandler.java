a095dd8304913918fd3f4df9dc4e335ebf2de70e
Dozer <mail@dozer.cc>
2018-11-30 10:07:41 +0800

    [Bug] Fix uuid convert failed bug on old version mysql.

--- src/main/java/com/narvii/server/mybatis/typehandler/UuidTypeHandler.java
+++ src/main/java/com/narvii/server/mybatis/typehandler/UuidTypeHandler.java
@@ -5,9 +5,6 @@ import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.MappedTypes;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
@@ -16,54 +13,42 @@ import java.sql.SQLException;
 import java.util.UUID;
 
 @MappedJdbcTypes(JdbcType.BINARY)
-@MappedTypes({ UUID.class })
+@MappedTypes({UUID.class})
 public class UuidTypeHandler extends BaseTypeHandler<UUID> {
 
-    InputStream getInputStream(UUID uuid) {
+    private byte[] getInputStream(UUID uuid) {
         ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
         bb.putLong(uuid.getMostSignificantBits());
         bb.putLong(uuid.getLeastSignificantBits());
-        return new ByteArrayInputStream(bb.array());
+        return bb.array();
     }
 
-    UUID getUuid(InputStream inputStream) throws IOException {
-        byte[] data = new byte[16];
-        int length = inputStream.read(data, 0, 16);
-        assert length == 16;
+    private UUID getUuid(byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        if (data.length != 16) {
+            throw new IllegalArgumentException("Length of byte array should be 16.");
+        }
         ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-        Long high = byteBuffer.getLong();
-        Long low = byteBuffer.getLong();
-        return new UUID(high, low);
-    }
-
-    UUID getUuid(byte[] data) {
-        ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-        Long high = byteBuffer.getLong();
-        Long low = byteBuffer.getLong();
+        long high = byteBuffer.getLong();
+        long low = byteBuffer.getLong();
         return new UUID(high, low);
     }
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
-        ps.setBinaryStream(i, getInputStream(parameter));
+        ps.setBytes(i, getInputStream(parameter));
     }
 
     @Override
     public UUID getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        try {
-            return getUuid(rs.getBinaryStream(columnName));
-        } catch (IOException exp) {
-            throw new SQLException(exp);
-        }
+        return getUuid(rs.getBytes(columnName));
     }
 
     @Override
     public UUID getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        try {
-            return getUuid(rs.getBinaryStream(columnIndex));
-        } catch (IOException exp) {
-            throw new SQLException(exp);
-        }
+        return getUuid(rs.getBytes(columnIndex));
     }
 
     @Override
